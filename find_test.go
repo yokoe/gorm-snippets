c01a4edbe9fe019210c6577fb5d0f90f76c3fdd5
Sota Yokoe <sota@kreuz45.com>
2019-07-04 22:08:22 +0900

    test: Table driven tests

--- find_test.go
+++ find_test.go
@@ -3,6 +3,7 @@ package snippet_test
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"testing"
 
@@ -11,25 +12,37 @@ import (
 
 var update = flag.Bool("update", false, "update .golden files")
 
-func TestFindByID(t *testing.T) {
-	goldenFile := "./testdata/FindByID.golden"
-	r, err := snippet.FindByID("model.Book")
-	if err != nil {
-		t.Fatalf("err = %v, want nil", err)
+func TestFindFunctions(t *testing.T) {
+	tests := []struct {
+		name         string
+		generateFunc func() (string, error)
+	}{
+		{"FindByID", func() (string, error) { return snippet.FindByID("model.Book") }},
+		{"FindByParam", func() (string, error) { return snippet.FindByParam("model.Book", "Title", "string") }},
+		{"BatchFindByID", func() (string, error) { return snippet.BatchFindByID("model.Book") }},
 	}
-	b := []byte(r)
-	if *update {
-		if err := ioutil.WriteFile(goldenFile, b, 0644); err != nil {
-			t.Fatalf("Failed to update golden file: %+v", err)
-		}
-	}
-	expected, err := ioutil.ReadFile(goldenFile)
-	if err != nil {
-		t.Fatalf("Failed to read golden file: %+v", err)
-	}
-	if !bytes.Equal(expected, b) {
-		t.Logf("Expect:\n%v", string(expected))
-		t.Logf("Got:\n%v", string(b))
-		t.Fatal("Result is not same to golden file.")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			goldenFile := fmt.Sprintf("./testdata/%s.golden", tt.name)
+			r, err := tt.generateFunc()
+			if err != nil {
+				t.Fatalf("err = %v, want nil", err)
+			}
+			b := []byte(r)
+			if *update {
+				if err := ioutil.WriteFile(goldenFile, b, 0644); err != nil {
+					t.Fatalf("Failed to update golden file: %+v", err)
+				}
+			}
+			expected, err := ioutil.ReadFile(goldenFile)
+			if err != nil {
+				t.Fatalf("Failed to read golden file: %+v", err)
+			}
+			if !bytes.Equal(expected, b) {
+				t.Logf("Expect:\n%v", string(expected))
+				t.Logf("Got:\n%v", string(b))
+				t.Fatal("Result is not same to golden file.")
+			}
+		})
 	}
 }
