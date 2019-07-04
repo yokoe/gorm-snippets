9ff19406ab5ad8a5f8fdaba3ed60f3ef65cacdf7
Sota Yokoe <sota@kreuz45.com>
2019-07-04 22:11:05 +0900

    test: Rename golden files.

--- find_test.go
+++ find_test.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/iancoleman/strcase"
 	"github.com/yokoe/gorm-snippets"
 )
 
@@ -23,7 +24,7 @@ func TestFindFunctions(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			goldenFile := fmt.Sprintf("./testdata/%s.golden", tt.name)
+			goldenFile := fmt.Sprintf("./testdata/%s.golden", strcase.ToSnake(tt.name))
 			r, err := tt.generateFunc()
 			if err != nil {
 				t.Fatalf("err = %v, want nil", err)
